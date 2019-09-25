819dc70cb4242a22bc3f1fb906227e4141cfabdb
Nick <nickmomotenkoforweb@gmail.com>
2019-09-25 15:40:35 +0300

    just test

---
+++ build/precache-manifest.49205a8cecf10a079ab54ea0ac1d1777.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce2cc815914f3b9717456d5e358ef89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331a905d90c251527dfa",
+    "url": "/static/css/main.ff4eb16c.chunk.css"
+  },
+  {
+    "revision": "b2e2d98b6fcd36106ece",
+    "url": "/static/js/2.22a411be.chunk.js"
+  },
+  {
+    "revision": "331a905d90c251527dfa",
+    "url": "/static/js/main.a8c80e1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b9134ee1d03545df29cf4ca136ec8ab",
+    "url": "/static/media/1.1b9134ee.jpg"
+  },
+  {
+    "revision": "56a6bcc5099c598fe180ab346b56d273",
+    "url": "/static/media/2.56a6bcc5.jpg"
+  },
+  {
+    "revision": "7c1ba147d4391d95a031a52de7af91b2",
+    "url": "/static/media/3.7c1ba147.jpg"
+  },
+  {
+    "revision": "b792f8a7554d333b24f3bba701921d32",
+    "url": "/static/media/map.b792f8a7.png"
+  },
+  {
+    "revision": "b66425a068c6a07783526cd7958a5d2f",
+    "url": "/static/media/user.b66425a0.jpg"
+  },
+  {
+    "revision": "823ac8793663ac32ef53e1e37710335e",
+    "url": "/static/media/wave1.823ac879.png"
+  }
+]);
